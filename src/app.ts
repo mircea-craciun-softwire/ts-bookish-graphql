92ae05d53cc93bf95298b922c30b49791561dfe7
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-11 15:21:13 +0300

    Setting up tedious, created queries for initialization

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,28 @@ import schema from './schemas/schema';
 import resolvers from './resolvers/resolvers';
 import { graphqlHTTP } from 'express-graphql';
 
+import { Request, Connection } from 'tedious';
+
 const server = express();
 
 const PORT = 3000;
+
+const config: any = {
+    server: '127.0.0.1',
+    authentication: {
+        type: 'ntlm',
+        options: {
+            domain: '',
+            userName: 'MirMeh',
+            password: '65urTVVx_Pn5NnB',
+        },
+    },
+    options: {
+        port: 1433,
+        database: 'Alexandria',
+        trustServerCertificate: true,
+    },
+};
 
 server.use(
     '/graphql',
@@ -19,3 +38,33 @@ server.use(
 server.listen(PORT, () => {
     console.log(`Server is running on localhost:${PORT}`);
 });
+
+const connection = new Connection(config);
+connection.connect((err) => {
+    if (err) {
+        console.log('Connection Failed');
+        throw err;
+    }
+    executeStatement();
+});
+function executeStatement() {
+    const request = new Request('SELECT * FROM Books', (err, rowCount) => {
+        if (err) {
+            throw err;
+        }
+        console.log('DONE!');
+        connection.close();
+    });
+    // Emits a 'DoneInProc' event when completed.
+    request.on('row', (columns) => {
+        columns.forEach((column) => {
+            if (column.value === null) {
+                console.log('NULL');
+            } else {
+                console.log(column.value);
+            }
+        });
+    });
+    // In SQL Server 2000 you may need: connection.execSqlBatch(request);
+    connection.execSql(request);
+}
--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -4,6 +4,25 @@ import { buildSchema } from 'graphql';
 const schema = buildSchema(`
     type Book {
         id: ID!
+        title: String!
+        author: String!
+        nrCopies: Int!
+    }
+    
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        password: String!
+        token: String
+    }
+    
+    type BorrowingHistory {
+        id: ID!
+        book: Book!
+        user: User!
+        dueDate: String!
+        returnedDate: String
     }
 
     type Query {
