4000e5d7323aa37a305f1ce0bf8ad410b6bad07a
mircea-craciun-softwire <Mircea.Craciun@softwire.com>
2024-07-11 15:25:45 +0300

    SQL Initialization queries

---
+++ queries/AlexandriaCreateTablesQueries.sql
@@ -0,0 +1,21 @@
+
+create table Alexandria.dbo.Books (
+	id int identity(1,1) primary key,
+	title varchar(100) not null,
+	author varchar(50) not null,
+	nrCopies int not null
+);
+create table Alexandria.dbo.Users (
+	id int identity(1,1) primary key,
+	[name] varchar(50) not null,
+	email varchar(50) not null,
+	[password] varchar(50) not null,
+	token varchar(10)
+);
+create table Alexandria.dbo.BorrowingHistory (
+	id int identity(1,1) primary key,
+	bookId int not null foreign key references Books(id),
+	userId int not null foreign key references Users(id),
+	dueDate date not null,
+	returnedDate date
+);
